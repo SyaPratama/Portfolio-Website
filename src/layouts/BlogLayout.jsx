657f896800fa3a77d80ab350aa8f862ab2f35730
RasyaPrtma <rasyaputraq17@gmail.com>
2024-03-25 08:45:40 +0700

    Add Blog File

---
+++ src/layouts/BlogLayout.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const BlogLayout = () => {
+    return(
+        <>
+            <h1 className="h1">BLOG</h1>
+        </>
+    );
+}
+
+export default BlogLayout
