5ad19e6022bf7a96c6cbdeb402fb1e1a98a312a7
RasyaPrtma <rasyaputraq17@gmail.com>
2024-04-03 09:04:25 +0700

    Change descriptiion to typewriter

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@
 import myFoto from '../assets/me.png';
 import Indonesia from '../assets/indonesia.png';
 import { Link } from 'react-router-dom';
+import { Typewriter } from 'react-simple-typewriter';
 const MainLayout = () => {
+    const img = <img src={Indonesia} width={20}/>;
+    const text = `Saya Adalah Fullstack Developer,Dan Saya Masih Seorang Siswa Menengah Kejuruan Di Salah Satu Kota Di Provinsi Jawa Tengah Yaitu Kota Batang.`;
     return(
         <>
                 <div className="main-text">
-                    <h1 className="introduce"><span>WELCOME</span><br/>I{"'"}Am A Fullstack Developer, My Name Is Rasya Putra Pratama</h1>
-                    <p className='description'>Saya Adalah Fullstack Developer Yang Berasal Dari  <img src={Indonesia} alt="indonesia" width={10}/>,Dan Saya Masih Seorang Siswa Menengah Kejuruan Di Salah Satu Kota Di Provinsi Jawa Tengah Yaitu Kota Batang.</p>
+                    <h1 className="introduce"><span>WELCOME</span><br/>I{"'"}Am A Fullstack Developer From {img} My Name Is Rasya Putra Pratama</h1>
+                    <div className='description'><Typewriter words={[text]} loop={0} typeSpeed={50} deleteSpeed={50} delaySpeed={1000}/></div>
                     <div className="social-media">
                         <Link className='icon' to={'https://www.instagram.com/hiyaaaaayam/'}>
                             <i className="fa-brands fa-instagram"></i>
